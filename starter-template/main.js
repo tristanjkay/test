c78549a2f0bda166f7a146f4ca84823200226ec7
tristanjkay <tristanjkay@gmail.com>
2021-06-24 12:33:48 +0100

    Initial Commit

---
+++ starter-template/main.js
@@ -0,0 +1,52 @@
+
+//VARIABLES
+var dropdown = document.getElementById("countriesDropdown");
+
+//POPULATE DROPDOWN
+
+if(window.location.pathname.includes("screen1.html")){
+        $.ajax({
+            url: "starter-template\geojson-fileget.php",
+            type: 'POST',  
+            dataType: 'json',
+            data: {
+    
+            },
+            success: function(result) {
+                console.log(result);
+                
+    
+                if (result.status.name == "ok") {
+                    console.log("OK");
+                    result['data'].forEach(element => {
+    
+                    var option = document.createElement("option");
+                    option.text = element.name;
+                    option.value = element.iso2;
+                    dropdown.add(option);
+                    console.log(element);
+                    });
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log("ERROR: geojson-fileget.php")
+            }
+        })
+    }
+
+
+//GET VALUE FROM DROPDOWN
+if(window.location.pathname.includes("screen1.html")){
+    dropdown.addEventListener("change", function() {
+        var selectedCountry = {"name": dropdown.value};
+    
+        localStorage.setItem('selectedCountry', JSON.stringify(selectedCountry));
+        console.log(localStorage.getItem("selectedCountry"));
+        window.location.replace("loading.html");
+    });
+}
+
+//LOAD DATA FROM APIS
+
+
+
