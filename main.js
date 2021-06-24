45ea91c70f7d1edbbf83d712f52c5b1dd2ac0e77
tristanjkay <tristanjkay@gmail.com>
2021-06-24 18:09:54 +0100

    update

--- main.js
+++ main.js
@@ -10,41 +10,29 @@ var selectedCountry;
             type: 'POST',  
             dataType: 'json',
             data: {
-    
             },
             success: function(result) {
                 
                 if (result.status.name == "ok") {
                     
                     result['data'].forEach(element => {
-                        
-    
                         var option = document.createElement("option");
                         option.text = element.properties.name;
                         option.value = element.properties.iso_a2;
                         dropdown.add(option);
                         geojsonResult[result['data'].indexOf(element)] = element;
-                       
-                       
-                    
                     });
-                    
-                    
-                     
                 }
-                 
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log("ERROR: geojson-fileget.php")
             }
         })
 
-
 //GET VALUE AND DATA FROM DROPDOWN/GEOJSON
     dropdown.addEventListener("change", function() {
         var name = dropdown.options[dropdown.selectedIndex].text;;
         var iso_a2 = dropdown.options[dropdown.selectedIndex].value;
-
         geojsonResult.forEach(element => {
             if(element.properties.name == name){
                 return selectedElement = element;
@@ -52,14 +40,15 @@ var selectedCountry;
         });
         var iso_a3 = selectedElement.properties.iso_a3;
         var geometry = selectedElement.geometry;
-
         selectedCountry = {"name": name, "iso_a2": iso_a2, "iso_a3": iso_a3, "geometry": geometry};
         localStorage.setItem('selectedCountry', JSON.stringify(selectedCountry));
-        window.location.replace("loading.html");
-        
+        console.log(localStorage.getItem('selectedCountry'['name']));
+        //window.location.replace("loading.html");
     });
 
 //LOAD DATA FROM APIS
+
+
 
 
 
