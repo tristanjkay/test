3d6218f8c0f917353bd60193d25a874bd5f11116
tristanjkay <tristanjkay@gmail.com>
2021-06-27 11:35:03 +0100

    update s2js

--- screen2.js
+++ screen2.js
@@ -8,8 +8,13 @@
 $('#ph_name').html(JSON.parse(localStorage.selectedCountry).name);
 $('#ph_capital').html(JSON.parse(localStorage.selectedCountry).capital);
 $('#ph_description').html(JSON.parse(localStorage.selectedCountry).description);
-$('#ph_currencyvalue').html(JSON.parse(localStorage.selectedCountry).capital);
+$('#ph_currencyvalue').html(JSON.parse(localStorage.selectedCountry).currencycode);
 $('#ph_currencycode').html(JSON.parse(localStorage.selectedCountry).currencycode);
+
+$('#ph_temperature').html(JSON.parse(localStorage.selectedCountry).weather.temperature + "&deg");
+$('#ph_feelslike').html("Feels like "+ JSON.parse(localStorage.selectedCountry).weather.feelslike + "&deg");
+
+
 
 
 
