cb545a6a20a2db98c94b54a57ed745b947c5ce09
tristanjkay <tristanjkay@gmail.com>
2021-06-27 11:50:50 +0100

    1

--- screen2.js
+++ screen2.js
@@ -5,18 +5,24 @@
 //POPULATE PAGE WITH DATA
 
 
+//General
 $('#ph_name').html(JSON.parse(localStorage.selectedCountry).name);
 $('#ph_capital').html(JSON.parse(localStorage.selectedCountry).capital);
 $('#ph_description').html(JSON.parse(localStorage.selectedCountry).description);
+
+//Economy
 $('#ph_currencyvalue').html(JSON.parse(localStorage.selectedCountry).currencycode);
 $('#ph_currencycode').html(JSON.parse(localStorage.selectedCountry).currencycode);
 
+
+//Weather
 $('#ph_temperature').html(JSON.parse(localStorage.selectedCountry).weather.temperature + "&deg");
 $('#ph_feelslike').html("Feels like "+ JSON.parse(localStorage.selectedCountry).weather.feelslike + "&deg");
 $('#ph_weathericon').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
 
 
+//Tourism
 
-
+$('#ph_tourismincome').html(JSON.parse(localStorage.selectedCountry).tourismincome);
 
 
