0123769c6da11ef4da48e4bb1c20bcf8a2cedef0
tristanjkay <tristanjkay@gmail.com>
2021-06-27 11:28:44 +0100

    Update screen2.js

--- screen2.js
+++ screen2.js
@@ -9,7 +9,7 @@ $('#ph_name').html(JSON.parse(localStorage.selectedCountry).name);
 $('#ph_capital').html(JSON.parse(localStorage.selectedCountry).capital);
 $('#ph_description').html(JSON.parse(localStorage.selectedCountry).description);
 $('#ph_currencyvalue').html(JSON.parse(localStorage.selectedCountry).capital);
-$('#ph_currencycode').html(JSON.parse(localStorage.selectedCountry).description);
+$('#ph_currencycode').html(JSON.parse(localStorage.selectedCountry).currencycode);
 
 
 
