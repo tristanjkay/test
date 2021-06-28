b8446c1e1670d5982c315193fd8548615e429e43
tristanjkay <tristanjkay@gmail.com>
2021-06-28 10:57:42 +0100

    1

---
+++ screen3.js
@@ -0,0 +1,33 @@
+
+//VARIABLES
+
+
+//POPULATE PAGE WITH DATA
+
+
+//General
+$('#ph_name').html(JSON.parse(localStorage.selectedCountry).name);
+$('#ph_capital').html(JSON.parse(localStorage.selectedCountry).capital);
+$('#ph_description').html(JSON.parse(localStorage.selectedCountry).description);
+
+//Economy
+$('#ph_currencyvalue').html(JSON.parse(localStorage.selectedCountry).currencycode);
+$('#ph_currencycode').html(JSON.parse(localStorage.selectedCountry).currencycode);
+
+
+//Weather
+$('#ph_temperature').html(JSON.parse(localStorage.selectedCountry).weather.temperature + "&deg");
+$('#ph_feelslike').html("Feels like "+ JSON.parse(localStorage.selectedCountry).weather.feelslike + "&deg");
+$('#ph_weathericon').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+$('#ph_humidity').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+$('#ph_precipitation').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+$('#ph_windspeed').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+$('#ph_winddirection').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+$('#ph_uvindex').attr("src", JSON.parse(localStorage.selectedCountry).weather.icon);
+
+
+//Tourism
+
+$('#ph_tourismincome').html(JSON.parse(localStorage.selectedCountry).tourismincome.value);
+$('#ph_flights').html(JSON.parse(localStorage.selectedCountry).arrivals.value);
+
