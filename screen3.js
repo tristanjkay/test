0fd4399c140f86baf16836b8e7e97602d463c198
tristanjkay <tristanjkay@gmail.com>
2021-06-28 18:39:03 +0100

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -26,7 +26,7 @@ $('#ph_winddirection').html(JSON.parse(localStorage.selectedCountry).weather.win
 $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 
 var index = 0;
-var daytemps = [];
+
 // chart colors
 var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
 //Forecast
@@ -34,7 +34,8 @@ JSON.parse(localStorage.selectedCountry).weather.forecast.forEach(element => {
     
     
     var string = "day[" +index+"]";
-    daytemps.push([element.hour[0].temp_c, element.hour[2].temp_c, element.hour[5].temp_c, element.hour[8].temp_c, element.hour[11].temp_c, element.hour[14].temp_c, element.hour[17].temp_c, element.hour[20].temp_c, element.hour[23].temp_c]);
+    var daytemps = [];
+    daytemps.push(element.hour[0].temp_c, element.hour[2].temp_c, element.hour[5].temp_c, element.hour[8].temp_c, element.hour[11].temp_c, element.hour[14].temp_c, element.hour[17].temp_c, element.hour[20].temp_c, element.hour[23].temp_c);
     index++;
 
     $('#forecast').append('<div class="row" style="margin-top: 2rem;"> <h3 style = "margin-bottom: 1rem;">This Week</h3> </div> <canvas id=' + string + ' width="0" style="padding-bottom:1rem"></canvas><div class="row "> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-right:.5rem; text-align: center;"> <h2>83</h2> <h6>Temperature</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>0</h2> <h6>Humidity</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>WSW</h2> <h6>Wind Speed</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>5.6</h2> <h6>Precipitation</h6> </div> </div>');
@@ -45,7 +46,7 @@ JSON.parse(localStorage.selectedCountry).weather.forecast.forEach(element => {
     labels: ["0:00", "02:00", "05:00", "08:00", "11:00", "14:00", "17:00", "20:00", "23:00"],
     datasets: [{
     label: "Temperature",
-    data: [daytemps[index][0], daytemps[index][1], daytemps[index][2], daytemps[index][3], daytemps[index][4], daytemps[index][5], daytemps[index][6], daytemps[index][7], daytemps[index][8]],
+    data: [daytemps[0], daytemps[1], daytemps[2], daytemps[3], daytemps[4], daytemps[5], daytemps[6], daytemps[7], daytemps[8]],
     backgroundColor: 'transparent',
     borderColor: colors[0],
     borderWidth: 4,
