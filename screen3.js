175d37b38abbbd8ae99c4cd338dc3a765f1c8011
tristanjkay <tristanjkay@gmail.com>
2021-06-28 12:18:07 +0100

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -25,9 +25,13 @@ $('#ph_windspeed').html(JSON.parse(localStorage.selectedCountry).weather.windSpe
 $('#ph_winddirection').html(JSON.parse(localStorage.selectedCountry).weather.windDirection);
 $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 
+var index = 0;
 //Forecast
 JSON.parse(localStorage.selectedCountry).weather.forecast.forEach(element => {
     $('#forecast').html('<div class="row" style="margin-top: 2rem;"> <h3 style = "margin-bottom: 1rem;">This Week</h3> </div> <canvas id="myChart" width="0" style="padding-bottom:1rem"></canvas><div class="row "> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-right:.5rem; text-align: center;"> <h2>83</h2> <h6>Temperature</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>0</h2> <h6>Humidity</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>WSW</h2> <h6>Wind Speed</h6> </div> <div class="col greyblock" style = "margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: center;"> <h2>5.6</h2> <h6>Precipitation</h6> </div> </div>');
+
+    console.log(index); 
+    index++;
 });
 
 //Weather Chart
