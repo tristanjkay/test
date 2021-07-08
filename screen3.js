753cbea3ab5b62f8510f434d4827ad00477fad4a
tristanjkay <tristanjkay@gmail.com>
2021-07-08 16:13:33 +0100

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -28,8 +28,8 @@ $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 //News
 
 JSON.parse(localStorage.selectedCountry).news.forEach(article => {
-    var formattedTime = (article.publishedAt.split("T")[1]).slice(0, -4);
-$('#articles_today').append("<div class='col greyblock' id='" + article.publishedAt + "' style = 'margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: left; min-width: 330px;'><h2 style='padding-left: 1rem; padding-bottom: 2rem;'>"+ article.title +"</h2><p style='padding-left: 1rem; padding-bottom: 1rem;'>"+ article.text +"</p><b style='margin-left: 1rem;'>"+ formattedTime +"</b></div>");
+    var formattedTime = (article.webPublicationDate.split("T")[1]).slice(0, -4);
+$('#articles_today').append("<div class='col greyblock' id='" + article.webPublicationDate + "' style = 'margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: left; min-width: 330px;'><h2 style='padding-left: 1rem; padding-bottom: 2rem;'>"+ article.webTitle +"</h2><p style='padding-left: 1rem; padding-bottom: 1rem;'>"+ article.text +"</p><b style='margin-left: 1rem;'>"+ formattedTime +"</b></div>");
   
 }
 )
