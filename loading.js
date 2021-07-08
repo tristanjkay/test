bf1ab002748f58074022d0a46c58b796062a3f3d
tristanjkay <tristanjkay@gmail.com>
2021-07-08 14:30:15 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -529,7 +529,7 @@ $.ajax({
 
             selectedCountry.news = result['data']['response']['results'];
             selectedCountry.news.forEach(article => {
-                $.get(article.webUrl, function(data){selectedCountry[article] = data.toString()})                
+                $.get(article.webUrl, function(data){article.content = data.toString()})                
             });
              }
 },
