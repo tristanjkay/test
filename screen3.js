64eda9fbd5db9f0e2c13050eb4ff950bccb29425
tristanjkay <tristanjkay@gmail.com>
2021-07-08 12:12:08 +0100

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -28,7 +28,7 @@ $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 //News
 
 JSON.parse(localStorage.selectedCountry).news.forEach(article => {
-  if(article.description.includes("Brazil") || article.title.includes("Brazil")){
+  if(article.description.includes(JSON.parse(localStorage.selectedCountry).name) || article.title.includes(JSON.parse(localStorage.selectedCountry).name)){
     var formattedTime = (article.publishedAt.split("T")[1]).slice(0, -4);
 $('#articles_today').append("<div class='col greyblock' id='" + article.publishedAt + "' style = 'margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: left; min-width: 330px;'><h2 style='padding-left: 1rem; padding-bottom: 2rem;'>"+ article.title +"</h2><p style='padding-left: 1rem; padding-bottom: 1rem;'>"+ article.description +"</p><b style='margin-left: 1rem;'>"+ formattedTime +"</b></div>");
   }
