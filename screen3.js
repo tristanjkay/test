042948a7c120daba9d2a7b8b20f345aa5df4bba8
tristanjkay <tristanjkay@gmail.com>
2021-07-08 17:10:52 +0100

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -28,7 +28,8 @@ $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 //News
 
 JSON.parse(localStorage.selectedCountry).news.forEach(article => {
-    var formattedTime = (article.webPublicationDate.split("T")[1]).slice(0, -4);
+    //var formattedTime = (article.webPublicationDate.split("T")[1]).slice(0, -4);
+    var formattedTime = article.webPublicationDate;
 $('#articles_today').append("<div class='col greyblock' id='" + article.webPublicationDate + "' style = 'margin:1rem; margin-top:.5rem; margin-left:.5rem; text-align: left; min-width: 330px;'><h2 style='padding-left: 1rem; padding-bottom: 2rem; min-height:12rem'>"+ article.webTitle +"</h2><p style='padding-left: 1rem; padding-bottom: 1rem; min-height:17rem;'>"+ article.text +"</p><b style='margin-left: 1rem;'>"+ formattedTime +"</b></div>");
   
 }
