f72284b050b0c101f77ca759f2be34b7a475b975
tristanjkay <tristanjkay@gmail.com>
2021-07-08 14:43:20 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -531,7 +531,7 @@ $.ajax({
             selectedCountry.news.forEach(article => {
                 $.ajax({
                     type:     "GET",
-                    url:      article.webTitle, // <-- Here
+                    url:      article.webTitle,
                     dataType: "jsonp",
                     success: function(data){
                         console.log(data);
