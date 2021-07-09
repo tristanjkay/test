830eb4b7d6f92c57d6d46a490d1a73a23401830d
tristanjkay <tristanjkay@gmail.com>
2021-07-09 18:44:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -67,6 +67,7 @@ var selectedCountry;
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log("ERROR: geojson-fileget.php")
+                localStorage.setItem("capitalfileget", "none");
             }
         });
     
