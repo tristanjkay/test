beec1ff1288780701dde0ab39295a56619421431
tristanjkay <tristanjkay@gmail.com>
2021-07-09 16:47:01 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,4 +1,4 @@
-selectedCountry = {"name": JSON.parse(localStorage.selectedCountry).name, "iso_a2": JSON.parse(localStorage.selectedCountry).iso_a2, "iso_a3": JSON.parse(localStorage.selectedCountry).iso_a3, "geometry": JSON.parse(localStorage.selectedCountry).geometry, "pois": {}};
+selectedCountry = {"name": JSON.parse(localStorage.selectedCountry).name, "iso_a2": JSON.parse(localStorage.selectedCountry).iso_a2, "iso_a3": JSON.parse(localStorage.selectedCountry).iso_a3, "geometry": JSON.parse(localStorage.selectedCountry).geometry, "centralPoint": JSON.parse(localStorage.selectedCountry).centralPoint};
 var mycountry = JSON.parse(localStorage.selectedCountry).iso_a2;
 var mycountryname = JSON.parse(localStorage.selectedCountry).name;
 
