6f4291fe035f376dbfbe0c27f5034c44f6f8ef8a
tristanjkay <tristanjkay@gmail.com>
2021-07-12 13:14:42 +0100

    Update pointsofinterest.php

--- php/culture/pointsofinterest.php
+++ php/culture/pointsofinterest.php
@@ -20,7 +20,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode['features'];
+	$output['data'] = $decode['results'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
