4e8badfaadc227bc3b4aa366d3f41e303c5840a9
tristanjkay <tristanjkay@gmail.com>
2021-07-15 10:58:46 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -480,7 +480,7 @@ imagesArray = [];
 //Images
 //https://serpapi.com/
 selectedCountry.pois.forEach(element => {
-    // element['poi']['name']
+    console.log("The name is" + element['poi']['name']);
     $.ajax({
         url: "php/general/photos.php",
         type: 'GET',
