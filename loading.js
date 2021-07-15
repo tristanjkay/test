9661e035331c3f3a4d4a2ae4103327bbd587d97f
tristanjkay <tristanjkay@gmail.com>
2021-07-15 10:43:44 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -497,7 +497,7 @@ selectedCountry.pois.forEach(element => {
 
     
             if (result.status.name == "ok") {
-                var pair = element['poi']['name'] + "^" + result[data];
+                var pair = element['poi']['name'] + "^" + result['data'];
                 imagesArray.push(pair)
                 console.log(imagesArray);
                 if(imagesArray.length == selectedCountry.pois.length){
