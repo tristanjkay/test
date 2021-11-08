48b8d84395c13fbfe2bbf83b536951397a44b14e
tristanjkay <tristanjkay@gmail.com>
2021-11-08 12:31:00 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -15,6 +15,7 @@ window.history.replaceState('','','/');
 //Globe
     selectedCountry = {"name": JSON.parse(localStorage.selectedCountry).name, "iso_a2": JSON.parse(localStorage.selectedCountry).iso_a2, "iso_a3": JSON.parse(localStorage.selectedCountry).iso_a3, "geometry": JSON.parse(localStorage.selectedCountry).geometry};
     var mycountry = JSON.parse(localStorage.selectedCountry).iso_a2;
+    var mycountry3 = JSON.parse(localStorage.selectedCountry).iso_a3;
     var mycountryname = JSON.parse(localStorage.selectedCountry).name;
 
 //Dates for lookups
@@ -121,7 +122,7 @@ percentLoaded = (ajaxSuccess/62)*100;
 
         if (result.status.name == "ok") {
             
-            console.log(result['data'][0]['currencies']["'" + JSON.parse(localStorage.selectedCountry).iso_a3 + "'"])
+            console.log(result['data'][0]['currencies']["'" + mycountry3 + "'"])
 
             //Set Data to Country Object
             selectedCountry.capital = result['data'][0]['capital'][0];
