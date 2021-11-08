3fa5e8e0956e6e578b793845d8a5e8d1fc4f16b7
tristanjkay <tristanjkay@gmail.com>
2021-11-08 12:18:48 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -121,7 +121,7 @@ percentLoaded = (ajaxSuccess/62)*100;
 
         if (result.status.name == "ok") {
 
-            console.log(result['data'][0]['currencies'][JSON.parse(localStorage.selectedCountry).iso_a3]['name'])
+            console.log(result['data'][0]['currencies'][JSON.parse(localStorage.selectedCountry).iso_a3])
 
             //Set Data to Country Object
             selectedCountry.capital = result['data'][0]['capital'][0];
