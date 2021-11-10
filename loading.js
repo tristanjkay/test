e8e8a4a07621b9f8b188cb337b605b0dfc0e2a26
tristanjkay <tristanjkay@gmail.com>
2021-11-10 18:25:16 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -136,7 +136,7 @@ percentLoaded = (ajaxSuccess/62)*100;
 
 for (const [key, value] of Object.entries(selectedCountry.currency)) {
     
-    return [selectedCountry.currencycode = (Object.values(selectedCountry.currency)[0].constructor.name +  " (" + value['symbol'] + ")") , selectedCountry.currencyname = value['name']]
+    return [selectedCountry.currencycode = (Object.values(selectedCountry.currency)[0] +  " (" + value['symbol'] + ")") , selectedCountry.currencyname = value['name']]
     //console.log("This is the value: ",value['name']);
 
   }
