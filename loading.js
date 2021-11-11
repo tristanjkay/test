e3ad7b35782ac6106ba3ab3d04893e98340c028b
tristanjkay <tristanjkay@gmail.com>
2021-11-11 13:11:27 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -133,7 +133,7 @@ percentLoaded = (ajaxSuccess/62)*100;
             selectedCountry.currencycode = result['data'][0]['currencies'];
             
             selectedCountry.currencycode = [[selectedCountry.currencycode].flat()][0][0];
-            console.log("LatLong: ", result['data'][0]['capitalInfo']['latlng']);
+            console.log("LatLong: ", result['data'][0]['capitalInfo']['latlng'][0]);
 
 for (const [key, value] of Object.entries(selectedCountry.currency)) {
     
