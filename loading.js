2797886400183afd370a62eda228a84f16e9ea0f
tristanjkay <tristanjkay@gmail.com>
2021-11-11 15:17:28 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -537,7 +537,8 @@ selectedCountry.pois.forEach(element => {
         type: 'GET',
         dataType: 'json',
         data: {
-            ref: element['photos'][0]['photo_reference'],
+            //ref: element['photos'][0]['photo_reference'],
+            ref:"Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT"
         },
         success: function(result) {
             console.log(result);
