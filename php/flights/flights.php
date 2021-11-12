379777c2b31cc87452ba59e4e11b4ff88aef051a
tristanjkay <tristanjkay@gmail.com>
2021-11-12 12:04:29 +0000

    Update flights.php

--- php/flights/flights.php
+++ php/flights/flights.php
@@ -2,27 +2,22 @@
 
 $curl = curl_init();
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "http://api.travelpayouts.com/v1/prices/direct?origin=MOW&destination=LED&depart_date=2021–11&token=c45e086415e768417990780a85fa0a13",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-
-	],
-]);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.travelpayouts.com/v2/prices/latest?currency=usd&page=1&limit=5&show_to_affiliates=true',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'X-Access-Token: c45e086415e768417990780a85fa0a13',
+    'Cookie: ab-live=false; marker_expires=30'
+  ),
+));
 
 $response = curl_exec($curl);
-$err = curl_error($curl);
 
 curl_close($curl);
-
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
-}
+echo $response;
