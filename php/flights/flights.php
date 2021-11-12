e4a0c62867a5dc827da54c9222fec8456cc24bcb
tristanjkay <tristanjkay@gmail.com>
2021-11-12 11:27:05 +0000

    Update flights.php

--- php/flights/flights.php
+++ php/flights/flights.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-	CURLOPT_URL => "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/GBP/en-US/LOND-sky/BSB-sky/anytime?inboundpartialdate=anytime",
+	CURLOPT_URL => "https://google-flights-search.p.rapidapi.com/search?departure_airport_code=SFO&arrival_airport_code=LAX",
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_ENCODING => "",
@@ -12,12 +12,12 @@ curl_setopt_array($curl, [
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	CURLOPT_CUSTOMREQUEST => "GET",
 	CURLOPT_HTTPHEADER => [
-		"x-rapidapi-host: skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		"x-rapidapi-host: google-flights-search.p.rapidapi.com",
 		"x-rapidapi-key: 459184f5c5mshec4ad720a01f413p1b89c7jsn7258217e5ff6"
 	],
 ]);
 
-$result = curl_exec($curl);
+$response = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
@@ -25,5 +25,5 @@ curl_close($curl);
 if ($err) {
 	echo "cURL Error #:" . $err;
 } else {
-	echo $result;
+	echo $response;
 }
