88a3bc39bac49ba63710761ddd420c2048919809
tristanjkay <tristanjkay@gmail.com>
2021-11-13 12:41:45 +0000

    Update screen2.js

--- screen2.js
+++ screen2.js
@@ -162,7 +162,7 @@ var map = L.map('map').fitWorld();
       zoomOffset: -1
     }).addTo(map);
 
-    map.addLayer(L.marker([JSON.parse(localStorage.getItem('capitalLocationLat')), JSON.parse(localStorage.getItem('capitalLocationLong'))]).bindPopup('<p class="popup__header"> Test</p>' ))
+    L.marker([JSON.parse(localStorage.getItem('capitalLocationLat')), JSON.parse(localStorage.getItem('capitalLocationLong'))]).bindPopup('<p class="popup__header"> Test</p>',{autoClose:false}).addTo(map)
     
     map.setView([JSON.parse(localStorage.getItem('capitalLocationLat')), JSON.parse(localStorage.getItem('capitalLocationLong'))], 5);
 
