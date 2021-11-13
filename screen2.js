29372c90b6205824635f834e5ee7b0cf577bb834
tristanjkay <tristanjkay@gmail.com>
2021-11-13 12:11:14 +0000

    Update screen2.js

--- screen2.js
+++ screen2.js
@@ -153,7 +153,7 @@ for (const [key, value] of Object.entries(top3newsitems[2])) {
 var map = L.map('map').fitWorld();
     
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      maxZoom: 18,
+      maxZoom: 7,
       minZoom: 5,
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
