be073150ecd20b392de8a82d6b1d6c853fd59d5c
tristanjkay <tristanjkay@gmail.com>
2021-11-14 18:27:50 +0000

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -294,13 +294,17 @@ setTimeout(function () {
         var poiArray = JSON.parse(localStorage.selectedCountry).pois;
 
         var markers = {};
+        var lastMarkerSelected=null;
 
         function clickPoi(id){
-          
+          if(lastMarkerSelected != null){
+            lastMarkerSelected.setIcon(placeIcon);
+          }
           map.setView([markers[id]['_latlng']['lat'],markers[id]['_latlng']['lng']], 16);
           markers[id].options['icon'].options['html'] = '<p style="font-size: 60px; color: red z-index: 999"> &#9733;</p>';
           console.log(markers[id].options['icon'].options['html']);
           markers[id].setIcon(capitalIcon);
+          return (lastMarkerSelected = marker[id]);
         }
 
 
