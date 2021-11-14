ec2846a64ccae6378836a27cf7023e3de9470663
tristanjkay <tristanjkay@gmail.com>
2021-11-14 19:31:12 +0000

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -240,11 +240,11 @@ setTimeout(function () {
       var map = L.map('map').fitWorld();
 
       const placeIcon = L.divIcon({
-        html: '<p style="font-size: 60px; z-index: 999; color: #235FB2; opacity: 0.5"> &#8226; </p>',
+        html: '<p style="font-size: 80px; z-index: 999; color: #235FB2; opacity: 0.5"> &#8226; </p>',
       });
 
       const selectedPlaceIcon = L.divIcon({
-        html: '<p style="font-size: 70px; color: #235FB2; z-index: 999; opacity: 1"> &#8226;</p>',
+        html: '<p style="font-size: 90px; color: #235FB2; z-index: 999; opacity: 1"> &#8226;</p>',
       });
 
       const capitalIcon = L.divIcon({
