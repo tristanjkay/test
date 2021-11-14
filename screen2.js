d04ce79cce7294e5106ebe0ba9e193d32d6428b3
tristanjkay <tristanjkay@gmail.com>
2021-11-14 11:52:00 +0000

    Update screen2.js

--- screen2.js
+++ screen2.js
@@ -153,7 +153,7 @@ for (const [key, value] of Object.entries(top3newsitems[2])) {
 var map = L.map('map').fitWorld();
 
       const fontAwesomeIcon = L.divIcon({
-        html: '<p style="font-size: 50px"> &#9733;</p>',
+        html: '<p style="font-size: 60px; z-index: 999"> &#9733;</p>',
       });
     
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
