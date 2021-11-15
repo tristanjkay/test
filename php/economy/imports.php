fe126978b64cd24f8d448cdf93f765e6ec502ec5
tristanjkay <tristanjkay@gmail.com>
2021-11-15 13:26:06 +0000

    Update imports.php

--- php/economy/imports.php
+++ php/economy/imports.php
@@ -1,35 +1,37 @@
 <?php
+// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
 require_once 'HTTP/Request2.php';
 
 $request = new Http_Request2('https://api.wto.org/timeseries/v1/data');
 $url = $request->getUrl();
 
 $headers = array(
- 
+    // Request headers
+    'Content-Type' => 'application/json',
     'Ocp-Apim-Subscription-Key' => '71e13c8a8030440e814fe17043f74a47',
 );
 
 $request->setHeader($headers);
 
 $parameters = array(
-    'i' => 'ITS_MTV_AM',
-    'r' => '036',
-    'fmt' => 'json',
-    'mode' => 'full',
-    'lang' => '1',
-    'meta' => 'true',
+    // Request parameters
 );
 
 $url->setQueryVariables($parameters);
 
-$request->setMethod(HTTP_Request2::METHOD_GET);
+$request->setMethod(HTTP_Request2::METHOD_POST);
 
-
+// Request body
 $request->setBody("{body}");
 
-$response = $request->send();
-echo $response->getBody();
-
-
+try
+{
+    $response = $request->send();
+    echo $response->getBody();
+}
+catch (HttpException $ex)
+{
+    echo $ex;
+}
 
 ?>
