726e044bc442694314df72af667aac79f4b9fb51
tristanjkay <tristanjkay@gmail.com>
2021-11-15 15:03:03 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -702,6 +702,9 @@ ajaxCount++;
 
 
 //Imports WTO
+    JSON.parse(localStorage.getItem('wtocodes')).forEach(element => {
+    console.log(element["name"]);    
+    });
     var params = {
         // Request parameters
         "i": "ITS_MTV_AM",
