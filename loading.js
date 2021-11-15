62fc63a40288d32a864550d5477456484352e2ed
tristanjkay <tristanjkay@gmail.com>
2021-11-15 15:33:28 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -13,7 +13,7 @@ window.history.replaceState('','','/');
     });
 
 //Globe
-    selectedCountry = {"name": JSON.parse(localStorage.selectedCountry).name, "iso_a2": JSON.parse(localStorage.selectedCountry).iso_a2, "iso_a3": JSON.parse(localStorage.selectedCountry).iso_a3, "geometry": JSON.parse(localStorage.selectedCountry).geometry, "flag": "", "wtocode": selectedCountry.wtocode = JSON.parse(localStorage.getItem('wtocode'))};
+    var selectedCountry = {"name": JSON.parse(localStorage.selectedCountry).name, "iso_a2": JSON.parse(localStorage.selectedCountry).iso_a2, "iso_a3": JSON.parse(localStorage.selectedCountry).iso_a3, "geometry": JSON.parse(localStorage.selectedCountry).geometry, "flag": "", "wtocode": selectedCountry.wtocode = JSON.parse(localStorage.getItem('wtocode'))};
     var mycountry = JSON.parse(localStorage.selectedCountry).iso_a2;
     var mycountry3 = JSON.parse(localStorage.selectedCountry).iso_a3;
     var mycountryname = JSON.parse(localStorage.selectedCountry).name;
