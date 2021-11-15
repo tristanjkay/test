6513de484f9d57c38205ecf5ccf122689b273238
tristanjkay <tristanjkay@gmail.com>
2021-11-15 14:35:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ $.ajax({
 })
 .done(function(data) {
     var parsedData = data;
-    localStorage.setItem("selectedCountry.wto", parsedData);
+    localStorage.setItem("selectedCountry.wto", JSON.stringify(parsedData));
 
 
  
