deed728d60db4295c7133da88748eef3fa07b44c
tristanjkay <tristanjkay@gmail.com>
2021-11-15 10:30:26 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -104,7 +104,7 @@ percentLoaded = (ajaxSuccess/62)*100;
 
         if (result.status.name == "ok") {
 
-            console.log(result['data'][0].flags);
+            console.log(result['data'][0].flags['png']);
            
 
             //Set Data to Country Object
