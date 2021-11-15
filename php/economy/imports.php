72fdb3692e1509c2a35788aa5a2e3f6daa5f5b8f
tristanjkay <tristanjkay@gmail.com>
2021-11-15 12:23:09 +0000

    Update imports.php

--- php/economy/imports.php
+++ php/economy/imports.php
@@ -20,7 +20,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode['Dataset'];
+	$output['data'] = $decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
