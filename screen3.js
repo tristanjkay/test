cc88722f8e11e377817cbec1eb813ff88659d584
tristanjkay <tristanjkay@gmail.com>
2021-11-17 10:51:52 +0000

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -72,7 +72,7 @@ $('#ph_uvindex').html(JSON.parse(localStorage.selectedCountry).weather.uvIndex);
 
 
 //News
-var news = (JSON.parse(localStorage.selectedCountry).news).sort();
+var news = (JSON.parse(localStorage.selectedCountry).news);
 var articleCount = [0,0,0];
 
 //Date folder
