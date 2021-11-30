3551bb87dcbad10c4768b8c260623f87be598656
tristanjkay <tristanjkay@gmail.com>
2021-11-30 13:13:53 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -415,8 +415,12 @@ percentLoaded = (ajaxSuccess/62)*100;
 
             var rawWikiData = result['data'];
             console.log(rawWikiData);
-           
-            selectedCountry.description = Object.entries(rawWikiData)[0][1]['extract'].split('. ')[0] + ".";
+            if(Object.entries(rawWikiData)[0][1]['extract'].split('. ')[0].length < 110){
+                selectedCountry.description = Object.entries(rawWikiData)[0][1]['extract'].split('. ')[0] + ". " + Object.entries(rawWikiData)[0][1]['extract'].split('. ')[1] + ".";
+            } else {
+                selectedCountry.description = Object.entries(rawWikiData)[0][1]['extract'].split('. ')[0] + ".";
+            }
+            
             selectedCountry.descriptionfull = Object.entries(rawWikiData)[0][1]['extract'].split("\n");
      
             
